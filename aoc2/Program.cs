f517bd254e6f66afbb2ca3cd44a38bc07def5be1
Arjan ten Hoeve <arjan.ten.hoeve@achmea.nl>
2020-12-04 23:37:25 +0100

    tweede deel ook opgelost

--- aoc2/Program.cs
+++ aoc2/Program.cs
@@ -34,7 +34,29 @@ namespace aoc2
                 Console.WriteLine($"{line} ----> {valid} ");
 
             }
-            Console.WriteLine($"validpasswordscount= {validpasswordscounts}");
+            Console.WriteLine($"validpasswordscount volgens eerste deel= {validpasswordscounts}");
+
+            int i;
+            int j;
+            char letter2;
+            char[] password2;
+            int validpasswordscounts2 = 0;
+            foreach (string line in File.ReadLines(@"C:\Repos\aoc2\aoc2\input\password.txt"))
+            {
+
+                hak = line.Split(delimiterChars);
+                i = int.Parse(hak[0])-1;
+                j = int.Parse(hak[1])-1;
+                letter2 = hak[2].ToCharArray()[0];
+                password2 = hak[4].ToCharArray();
+
+                
+                valid = password2[i]==letter2^password2[j]==letter2 ;
+                if (valid) validpasswordscounts2++;
+                Console.WriteLine($"{line} ----> {valid} ");
+
+            }
+            Console.WriteLine($"validpasswordscount volgens tweede deel= {validpasswordscounts2}");
 
         }
     }
